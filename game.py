9b669f76420b298641deee99520892a64f33e749
Julian Mican <julian.mican@gmail.com>
2017-06-27 08:16:38 -0500

    Adjust the input for error in fuction activation ready for train

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ from random import randrange, sample
 from neat import nn, population, statistics
 
 parser = argparse.ArgumentParser(description='OpenAI Gym Solver')
-parser.add_argument('--max-steps', dest='max_steps', type=int, default=1000,
+parser.add_argument('--max-steps', dest='max_steps', type=int, default=5000,
                     help='The max number of steps to take per genome (timeout)')
 parser.add_argument('--episodes', type=int, default=1,
                     help="The number of times to run a single genome. This takes the fitness score from the worst run")
@@ -21,22 +21,30 @@ local_dir = os.path.dirname(__file__)
 config_path = os.path.join(local_dir, 'game_config')
 pop = population.Population(config_path)
 
-def simulate_species(net, episodes=1, steps=1000):
+def simulate_species(net, episodes=1, steps=5000):
     fitnesses = []
     for runs in range(episodes):
-        my_env = gym.make('ppaquette/meta-SuperMarioBros-v0')
+        my_env = gym.make('ppaquette/meta-SuperMarioBros-Tiles-v0')
         inputs = my_env.reset()
+
         cum_reward = 0.0
+        cont = 0;
         for j in range(steps):
-            outputs = net.serial_activate(inputs)
-            actions = get_actions(outputs)
+            inputs = inputs.flatten()
+            print(inputs)
+            # outputs = net.serial_activate(inputs)
+            # actions = get_actions(outputs)
+            actions
             inputs, reward, is_finished, info = my_env.step(actions)
             cum_reward_before = cum_reward
-            cum_reward = cum_reward + reward
-            if cum_reward_before > cum_reward:
+            cum_reward = info["total_reward"]
+            if (cum_reward_before - 3) > cum_reward:
+                break
+            if cum_reward_before == cum_reward:
+                cont = cont + 1
+            if cont == 100:
                 break
             my_env.render()
-
         fitnesses.append(cum_reward)
 
     fitness = np.array(fitnesses).mean()
@@ -58,6 +66,10 @@ def actions_is_active(actions):
         if actions[i] == 1:
             return True
     return False
+
+def worker_evaluate_genome(g):
+    net = nn.create_feed_forward_phenotype(g)
+    return simulate_species(net, my_env, args.episodes, args.max_steps, render=args.render)
 
 def train_network():
     def evaluate_genome(g):
